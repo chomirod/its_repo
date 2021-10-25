20d32085f6cc6dbd6ae236b02c81edb922fbd37f
chomirod <chomita08@gmail.com>
2021-10-25 20:49:59 -0300

    eee

--- socket/src/index.js
+++ socket/src/index.js
@@ -1,7 +1,11 @@
 const express = require ('express')
 const cors = require('cors')
 const socketio = require ('socket.io')
+const os = require('node-os-utils')
 
+
+//acceder a los recursos
+const cpu = os.cpu
 const server = express()
 
 server.set('port', process.env.PORT || 3000)
@@ -21,7 +25,23 @@ io.on('connection', (socket) => {
 
     setInterval(() =>{
 
-    socket.emit('dato-socket',Math.random())
+    socket.emit('dato-socket',{
+        dato:Math.random(),
+        nombre:'Romina'
+    })
+    }, 1000)
+
+
+    setInterval(() =>{
+        cpu.free()
+        .then(info =>{
+            socket.emit('datos-cpu',
+                {
+                    descripcion:'CPU FREE',
+                    data:info
+                }
+            )
+        });
     }, 1000)
 
     socket.on('respuesta', (valor) => {
